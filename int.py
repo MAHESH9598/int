3ee28650f844ceb3b5fbfa8aaa9d9f460773feb8
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-15 19:11:43 +0530

    Initial commit

---
+++ int.py
@@ -0,0 +1,44 @@
+#this is Example for ------------>int
+
+""" Definition : --> int is class & it represent a fundamental datatype.
+
+                 --> int type of Variables are called objects.
+
+                 --> int datatype allows us to represent 4 Number system Data.
+
+a)Decimal number system data:
+                 (0....9   base-10--default)
+
+b)Binary number system data:
+                 (0,1   base-2-- must be represent with 0b or 0B )
+
+c)Octal number system data:
+                 (0....7   base-8-- must be represent with 0o or 0O )
+
+d)Hexa number system data:
+                 (0....9, A....F   base-16-- must be represent with 0x or 0X )
+
+"""
+
+######## Examples :
+
+a=12349
+b=0B0101
+c=0O12347
+d=0X1234adf
+
+print("The Decimal Number System     12349   of a value is         =",a)
+print("The Binary Number System      0101    of b value is         =",b)
+print("The Octal Number System       12347   of c value is         =",c)
+print("The HexaDecimal Number System 1234adf of d value is         =",d)
+
+print("-"*40)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+
+
+
